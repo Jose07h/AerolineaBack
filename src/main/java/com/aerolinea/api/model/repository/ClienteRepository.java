21b9512c8ac4b0ccfe52a7ffbf1a480a15602edf
Jose luis <joseluis07h@gmail.com>
2020-04-23 10:18:32 -0500

    Se agregan nuevas consultas

--- src/main/java/com/aerolinea/api/model/repository/ClienteRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/ClienteRepository.java
@@ -2,9 +2,11 @@ package com.aerolinea.api.model.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.aerolinea.api.model.entity.Cliente;
+import com.aerolinea.api.model.entity.Equipaje;
 
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
 	public List<Cliente> findByNombre(String nombre);
@@ -13,4 +15,15 @@ public interface ClienteRepository extends CrudRepository<Cliente, Long> {
 
 	public List<Cliente> findByapMaterno(String apellido);
 
+	public List<Cliente> findByEqupajeIn(List<Equipaje> equpaje);
+	
+	public List<Cliente> findByCorreoLike(String correo);
+	
+	public List<Cliente> findByTelefono(Integer telefono);
+	
+	@Query(value = "select * from clientes :orden",nativeQuery = true)
+	public List<Cliente> findByOrden(String orden);
+	@Query(value = "SELECT DISTINCT C.* FROM CLIENTES C,EQUIPAJE E WHERE E.ID_CLIENTE=C.ID AND E.ID= ?1 ; ",nativeQuery = true)
+	public Cliente findByEquipajeId(Long id);
 }
+ 
