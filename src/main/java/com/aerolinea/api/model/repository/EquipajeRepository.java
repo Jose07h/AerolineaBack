51af223e11d29a0d17816fdd5c3306024f9b9ecf
Jose luis <joseluis07h@gmail.com>
2020-04-23 10:26:41 -0500

    Se agregan nuevas consultas para equipajeReposiory

--- src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
@@ -1,10 +1,15 @@
 package com.aerolinea.api.model.repository;
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import com.aerolinea.api.model.entity.Equipaje;;
+import com.aerolinea.api.model.entity.Equipaje;
+import com.aerolinea.api.service.utils.constants.TablesAndAttributesName;;
 
 public interface EquipajeRepository extends CrudRepository<Equipaje, Long>{
-	
+	@Query(value = "select * from "+TablesAndAttributesName.EQUIPAJE+"where id_cliente=?1",nativeQuery = true)
+	public List<Equipaje>findByClienteId(Long id); 
 }
