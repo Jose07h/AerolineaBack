2c381163643b66ea1fe6092726cb1fbf34cf5450
Jose luis <joseluis07h@gmail.com>
2020-04-23 12:49:01 -0500

    Se agrega nuevas consultas

--- src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
@@ -1,6 +1,5 @@
 package com.aerolinea.api.model.repository;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +8,11 @@ import org.springframework.data.repository.CrudRepository;
 import com.aerolinea.api.model.entity.Equipaje;
 import com.aerolinea.api.service.utils.constants.TablesAndAttributesName;;
 
-public interface EquipajeRepository extends CrudRepository<Equipaje, Long>{
-	@Query(value = "select * from "+TablesAndAttributesName.EQUIPAJE+"where id_cliente=?1",nativeQuery = true)
-	public List<Equipaje>findByClienteId(Long id); 
+public interface EquipajeRepository extends CrudRepository<Equipaje, Long> {
+	@Query(value = "select * from " + TablesAndAttributesName.EQUIPAJE + "where id_cliente=?1", nativeQuery = true)
+	public List<Equipaje> findByClienteId(Long id);
+
+	@Query(value = "SELECT * FROM " + TablesAndAttributesName.EQUIPAJE
+			+ "WHERE id_cliente=?1 AND ID=?2", nativeQuery = true)
+	public Equipaje findByIdAndEquipajeById(Long id, Long idEquipaje);
 }
