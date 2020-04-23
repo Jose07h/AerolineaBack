b2125fc5bc4afff3a43f0589eebe1d6eeb26c8d7
Jose luis <joseluis07h@gmail.com>
2020-04-23 10:30:07 -0500

    Se agregan nuevas consultas a vuelos repository

--- src/main/java/com/aerolinea/api/model/repository/ReservasVueloRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/ReservasVueloRepository.java
@@ -1,11 +1,16 @@
 package com.aerolinea.api.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.aerolinea.api.model.entity.Cliente;
 import com.aerolinea.api.model.entity.ReservasVuelo;
 
 
 
 public interface ReservasVueloRepository extends CrudRepository<ReservasVuelo, Long>{
-
+	public List
+	<ReservasVuelo> findByCliente(Cliente cliente);
+	
 }
