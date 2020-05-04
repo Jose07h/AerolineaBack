871530fbc7792b1744b4f4d41d2498ed4e4bccb4
Jose luis <joseluis07h@gmail.com>
2020-05-04 14:19:37 -0500

    Se actualizaon consultas

--- src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/EquipajeRepository.java
@@ -9,10 +9,11 @@ import com.aerolinea.api.model.entity.Equipaje;
 import com.aerolinea.api.service.utils.constants.TablesAndAttributesName;;
 
 public interface EquipajeRepository extends CrudRepository<Equipaje, Long> {
-	@Query(value = "select * from " + TablesAndAttributesName.EQUIPAJE + "where id_cliente=?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM " + TablesAndAttributesName.EQUIPAJE + "WHERE id_cliente=?1", nativeQuery = true)
 	public List<Equipaje> findByClienteId(Long id);
 
 	@Query(value = "SELECT * FROM " + TablesAndAttributesName.EQUIPAJE
 			+ "WHERE id_cliente=?1 AND ID=?2", nativeQuery = true)
 	public Equipaje findByIdAndEquipajeById(Long id, Long idEquipaje);
+	
 }
