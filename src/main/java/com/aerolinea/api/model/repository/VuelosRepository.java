67da7166c97040dfc4c057f9cbd8e9408ac2a451
Jose luis <joseluis07h@gmail.com>
2020-05-03 19:38:08 -0500

    Se agregan nuevas consultas

--- src/main/java/com/aerolinea/api/model/repository/VuelosRepository.java
+++ src/main/java/com/aerolinea/api/model/repository/VuelosRepository.java
@@ -1,5 +1,8 @@
 package com.aerolinea.api.model.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,6 +12,9 @@ import com.aerolinea.api.model.entity.Vuelo;
 
 public interface VuelosRepository extends CrudRepository<Vuelo, Long> {
 
+	@Query(value = "SELECT v,d,a FROM Vuelo v join v.destino d join v.avion a ")
+	public List<Vuelo> findAll();
+	
 	public Integer countByDestino(Destino destino);
 
 	public Integer countByAvion(Avion avion);
@@ -16,4 +22,14 @@ public interface VuelosRepository extends CrudRepository<Vuelo, Long> {
 	@Query(value = "SELECT COUNT(1) FROM personal_vuelo WHERE id_empleado=?1",nativeQuery = true)
 	public Integer countByEmpleadoId(Long id);
 	
+	@Query(value = "SELECT v,d,a FROM Vuelo v join v.destino d join v.avion a  WHERE d.pais=?1")
+	public List<Vuelo> findByDestinoPais(String pais);
+	
+	@Query(value = "SELECT v,d,a FROM Vuelo v join v.destino d join v.avion a  WHERE d.ciudad=?1")
+	public List<Vuelo> findByDestinoCiudad(String ciudad);
+	
+	public List<Vuelo> findByFechaAndDestino(Destino destino ,LocalDate date);
+	
+	public List<Vuelo> findByDestino(Destino destino);
+
 }
